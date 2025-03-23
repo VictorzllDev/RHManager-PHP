f390d4a96a4e000a88a55b97b737cdc614b5ac3f
VictorzllDev <VictorzllDev@gmail.com>
2025-03-23 18:28:16 -0300

    corrigindo error de semantica na view

--- app/application/views/employees/index.php
+++ app/application/views/employees/index.php
@@ -141,44 +141,44 @@
           </div>
         </div>
       </div>
-    </div>
-    <div class="overflow-x-auto">
-      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" class="px-4 py-3">Nome</th>
-            <th scope="col" class="px-4 py-3">Cargo</th>
-            <th scope="col" class="px-4 py-3">Departamento</th>
-            <th scope="col" class="px-4 py-3">CPF</th>
-            <th scope="col" class="px-4 py-3">Email</th>
-            <th scope="col" class="px-4 py-3">Telefone</th>
-            <th scope="col" class="px-4 py-3">Tipo</th>
-            <th scope="col" class="px-4 py-3">
-              <span class="sr-only">Actions</span>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach ($employees as $employee) : ?>
-            <tr class="border-b dark:border-gray-700">
-              <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"><?= $employee->name ?></th>
-              <td class="px-4 py-3"><?= $employee->position ?></td>
-              <td class="px-4 py-3"><?= $employee->department ?></td>
-              <td class="px-4 py-3"><?= $employee->cpf ?></td>
-              <td class="px-4 py-3"><?= $employee->email ?></td>
-              <td class="px-4 py-3"><?= $employee->phone ?></td>
-              <td class="px-4 py-3"><?= $employee->role ?></td>
-              <td class="px-4 py-3 flex items-center justify-end">
-                <button id="employee-options-button" onclick='openModal("modalEditEmployee", <?= json_encode($employee) ?>)' class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
-                  <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                  </svg>
-                </button>
-              </td>
+      <div class="overflow-x-auto">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-4 py-3">Nome</th>
+              <th scope="col" class="px-4 py-3">Cargo</th>
+              <th scope="col" class="px-4 py-3">Departamento</th>
+              <th scope="col" class="px-4 py-3">CPF</th>
+              <th scope="col" class="px-4 py-3">Email</th>
+              <th scope="col" class="px-4 py-3">Telefone</th>
+              <th scope="col" class="px-4 py-3">Tipo</th>
+              <th scope="col" class="px-4 py-3">
+                <span class="sr-only">Actions</span>
+              </th>
             </tr>
-          <?php endforeach; ?>
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            <?php foreach ($employees as $employee) : ?>
+              <tr class="border-b dark:border-gray-700">
+                <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"><?= $employee->name ?></th>
+                <td class="px-4 py-3"><?= $employee->position ?></td>
+                <td class="px-4 py-3"><?= $employee->department ?></td>
+                <td class="px-4 py-3"><?= $employee->cpf ?></td>
+                <td class="px-4 py-3"><?= $employee->email ?></td>
+                <td class="px-4 py-3"><?= $employee->phone ?></td>
+                <td class="px-4 py-3"><?= $employee->role ?></td>
+                <td class="px-4 py-3 flex items-center justify-end">
+                  <button id="employee-options-button" onclick='openModal("modalEditEmployee", <?= json_encode($employee) ?>)' class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
+                    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                    </svg>
+                  </button>
+                </td>
+              </tr>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </section>
